9b13a158113e6132d1179c67c093f7ce2d8ab65d
Homer Simpson <hb@hb.gl>
2022-11-13 17:50:10 +0100

    janky intergration with systray. volume not working on win11

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os/signal"
 	"strings"
 
+	"github.com/getlantern/systray"
 	"github.com/go-ole/go-ole"
 	"github.com/moutend/go-wca/pkg/wca"
 )
@@ -23,6 +24,13 @@ func main() {
 }
 
 func run(args []string) error {
+	systray.Run(onReady, onExit)
+
+	return nil
+}
+
+func setupAudio() error {
+
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt)
 
@@ -61,7 +69,6 @@ func run(args []string) error {
 		*/
 	}
 	fmt.Println("Done")
-
 	return nil
 }
 
@@ -124,4 +131,101 @@ func onDeviceStateChanged(pwstrDeviceId string, dwNewState uint64) error {
 func onPropertyValueChanged(pwstrDeviceId string, key uint64) error {
 	//fmt.Printf("Called OnPropertyValueChanged\t(%q, %v)\n", pwstrDeviceId, key)
 	return nil
+}
+
+func onReady() {
+	icon, err := os.ReadFile("icon.ico")
+	systray.SetTemplateIcon(icon, icon)
+	systray.SetTitle("Awesome App")
+	systray.SetTooltip("Lantern")
+	mQuitOrig := systray.AddMenuItem("Quit", "Quit the whole app")
+	go func() {
+		<-mQuitOrig.ClickedCh
+		fmt.Println("Requesting quit")
+		systray.Quit()
+		fmt.Println("Finished quitting")
+	}()
+
+	// We can manipulate the systray in other goroutines
+	go func() {
+		if err != nil {
+			log.Panic(err.Error())
+		}
+		systray.SetTemplateIcon(icon, icon)
+		systray.SetTitle("Awesome App")
+		systray.SetTooltip("Pretty awesome棒棒嗒")
+		mChange := systray.AddMenuItem("Change Me", "Change Me")
+		mChecked := systray.AddMenuItemCheckbox("Unchecked", "Check Me", true)
+		mEnabled := systray.AddMenuItem("Enabled", "Enabled")
+		// Sets the icon of a menu item. Only available on Mac.
+		mEnabled.SetTemplateIcon(icon, icon)
+
+		systray.AddMenuItem("Ignored", "Ignored")
+
+		subMenuTop := systray.AddMenuItem("SubMenuTop", "SubMenu Test (top)")
+		subMenuMiddle := subMenuTop.AddSubMenuItem("SubMenuMiddle", "SubMenu Test (middle)")
+		subMenuBottom := subMenuMiddle.AddSubMenuItemCheckbox("SubMenuBottom - Toggle Panic!", "SubMenu Test (bottom) - Hide/Show Panic!", false)
+		subMenuBottom2 := subMenuMiddle.AddSubMenuItem("SubMenuBottom - Panic!", "SubMenu Test (bottom)")
+
+		mUrl := systray.AddMenuItem("Open UI", "my home")
+		mQuit := systray.AddMenuItem("退出", "Quit the whole app")
+
+		// Sets the icon of a menu item. Only available on Mac.
+		mQuit.SetIcon(icon)
+
+		systray.AddSeparator()
+		mToggle := systray.AddMenuItem("Toggle", "Toggle the Quit button")
+		shown := true
+		toggle := func() {
+			if shown {
+				subMenuBottom.Check()
+				subMenuBottom2.Hide()
+				mQuitOrig.Hide()
+				mEnabled.Hide()
+				shown = false
+			} else {
+				subMenuBottom.Uncheck()
+				subMenuBottom2.Show()
+				mQuitOrig.Show()
+				mEnabled.Show()
+				shown = true
+			}
+		}
+
+		for {
+			select {
+			case <-mChange.ClickedCh:
+				mChange.SetTitle("I've Changed")
+			case <-mChecked.ClickedCh:
+				if mChecked.Checked() {
+					mChecked.Uncheck()
+					mChecked.SetTitle("Unchecked")
+				} else {
+					mChecked.Check()
+					mChecked.SetTitle("Checked")
+				}
+			case <-mEnabled.ClickedCh:
+				mEnabled.SetTitle("Disabled")
+				mEnabled.Disable()
+			case <-mUrl.ClickedCh:
+				fmt.Println("github.com/open-golang/open")
+				//open.Run("https://www.getlantern.org")
+			case <-subMenuBottom2.ClickedCh:
+				panic("panic button pressed")
+			case <-subMenuBottom.ClickedCh:
+				toggle()
+			case <-mToggle.ClickedCh:
+				toggle()
+			case <-mQuit.ClickedCh:
+				systray.Quit()
+				fmt.Println("Quit2 now...")
+				return
+			}
+		}
+	}()
+	setupAudio()
+}
+
+func onExit() {
+	fmt.Printf("systray custom exit")
 }
